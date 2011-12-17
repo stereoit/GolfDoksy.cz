b289b9fe5846ca699d6f8941ebcfc99261bbab32
Robert Smol <robert.smol@stereoit.com>
2011-12-17 01:02:41 +0100

    Added static files app

--- src/golfdoksy/settings.py
+++ src/golfdoksy/settings.py
@@ -72,7 +72,8 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = rel_path('static')
+#STATIC_ROOT = rel_path('/static/')
+STATIC_ROOT = rel_path('final_static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -88,6 +89,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    rel_path('static/'),
 )
 
 # List of finder classes that know how to find static files in
--- src/golfdoksy/urls.py
+++ src/golfdoksy/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.contrib import admin
 
@@ -17,4 +18,4 @@ if settings.DEBUG:
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     url(r'', include('django.contrib.staticfiles.urls')),
-) + urlpatterns
+) + urlpatterns + staticfiles_urlpatterns()
