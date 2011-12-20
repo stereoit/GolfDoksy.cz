66515ca6df07b51684407aca237a8c3ffa274289
Robert Smol <robert.smol@stereoit.com>
2011-12-20 22:41:24 +0100

    Added marker

---
+++ src/golfdoksy/static/js/golfdoksy.js
@@ -0,0 +1,35 @@
+$('document').ready(function() {
+    $.localScroll();
+
+    var scrolls, paddings,
+        els = $("#logo, #menu a"),
+        marker = $("#marker");
+    $(window).scroll(function(){
+        var scroll = $(this).scrollTop();
+        for(var i in scrolls){
+            if ( scrolls[i] > scroll-100 ) break;
+        }
+        marker.css({left: paddings[i]+"px"});
+        els.removeClass("active").eq(i).addClass("active");
+    }); 
+
+    $(window).resize(function(){
+        scrolls = [], paddings = [];
+        els.each(function(i){
+            paddings.push( $(this).offset().left + $(this).width()/2 + (i==0 ? -16 : 0) );
+            var id = $(this).attr("href");
+            scrolls.push( $(id).offset().top );
+        });
+        scrolls[1] = scrolls[1] + 200;
+        $(window).trigger("scroll");
+    }).trigger("resize");
+
+    els.click(function(e){
+        e.preventDefault();
+        var id = $(this).attr("href");
+        $('html,body').addClass("scrolling").animate({
+            scrollTop: $(id).offset().top + "px"
+        },1000).removeClass("scrolling");
+        return false;
+    }); 
+});
