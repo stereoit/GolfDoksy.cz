429768b84da788d9bd8e50dc0a3c3cc4438da0d9
Vašek Král <vasa81@gmail.com>
2012-03-14 10:47:19 +0100

    scroll js fixed

--- src/static/js/golfdoksy.js
+++ src/static/js/golfdoksy.js
@@ -1,5 +1,7 @@
 $('document').ready(function() {
-    $.localScroll();
+    header_height = 46;
+    
+    //$.localScroll();
 
     var scrolls, paddings,
         els = $("#logo, #menu a"),
@@ -18,7 +20,7 @@ $('document').ready(function() {
         els.each(function(i){
             paddings.push( $(this).offset().left + $(this).width()/2 + (i==0 ? -16 : 0) );
             var id = $(this).attr("href");
-            scrolls.push( $(id).offset().top );
+            scrolls.push( $(id).offset().top + header_height);
         });
         scrolls[1] = scrolls[1] + 200;
         $(window).trigger("scroll");
@@ -27,8 +29,9 @@ $('document').ready(function() {
     els.click(function(e){
         e.preventDefault();
         var id = $(this).attr("href");
+        console.log($(id).offset().top - header_height);
         $('html,body').addClass("scrolling").animate({
-            scrollTop: $(id).offset().top + "px"
+            scrollTop: $(id).offset().top - header_height + "px"
         },1000).removeClass("scrolling");
         return false;
     }); 
