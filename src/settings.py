9ed2e7cab3f8b0c745d073056a7d8ddd8db08530
Robert Smol <robert.smol@stereoit.com>
2012-03-15 13:15:32 +0100

    Logging

--- src/settings.py
+++ src/settings.py
@@ -196,26 +196,70 @@ THUMBNAIL_PROCESSORS = (
     'easy_thumbnails.processors.filters',
 )
 
+LOG_BASE_PATH = os.path.join(os.path.split(os.path.split(PROJECT_ROOT)[0])[0], 'logs')
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(asctime)s %(levelname)s %(message)s'
+        },
+    },
     'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler'
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
         'mail_admins': {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'file_all': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'simple',
+            'filename': os.path.join(LOG_BASE_PATH, 'all.log'),
+            'mode': 'a'
+        },
+        'file_sql': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'simple',
+            'filename': os.path.join(LOG_BASE_PATH, 'sql.log'),
+            'mode': 'a'
+        },
+
     },
     'loggers': {
+        '': {
+            'handlers': ['file_all',],
+            'level': 'DEBUG'
+        },
+        'django': {
+            'handlers':['null'],
+            'propagate': True,
+            'level':'INFO',
+        },
         'django.request': {
             'handlers': ['mail_admins'],
-            'level': 'ERROR',
             'propagate': True,
+            'level': 'ERROR',
+        },
+        'django.db.backends': {
+            'propagate': False,
+            'handlers': ['file_sql'],
+            'level': 'DEBUG'
         },
     }
 }
+
